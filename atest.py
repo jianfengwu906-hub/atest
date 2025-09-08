6ef9f7c4ad11b2493bb9012f35c71115ed1e0622
jianfengwu906-hub <jianfengwu906@gmail.com>
2025-09-08 16:06:06 +0800

    Create atest.py

---
+++ atest.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+url = "https://www.baidu.com"
+
+try:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+            result = response.text
+            try:
+                start = result.find('{')
+                end = result.rfind('}') + 1
+                json_str = result[start:end]
+                result=json.loads(json_str)
+            except (ValueError, AttributeError) as e:
+                json_str=None
+                
+    else:
+            print(f"请求失败，状态码：{response.status_code}")
+            print("响应内容：", response.text)
+except requests.exceptions.RequestException as e:
+        print(f"请求异常：{str(e)}")
+except json.JSONDecodeError:
+        print("响应内容非JSON格式：", response.text)
